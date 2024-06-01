254d7b027abf9363af6228a81687d46e6b6f13f6
TAWIAH GODFRED <103007384+10950761@users.noreply.github.com>
2024-06-01 14:48:14 +0000

    Add files via upload

---
+++ TicketPriceCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+public class TicketPriceCalculator
+{
+    public static void Main(String[] arg){
+        Console.WriteLine("Enter you age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+String ticketPrice;
+if (age >= 65 || age <= 12){
+    ticketPrice = "GHC7.00";
+}
+else {
+    ticketPrice = "GHC10.00";
+}
+Console.WriteLine("Ticket price: " + ticketPrice);
+    }
+}
